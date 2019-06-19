5851d804b1ca407d2d7fe8551425c78e491c9dcc
Diego Salgado <dasalgadob@unal.edu.co>
2019-06-19 11:55:15 -0500

    Devise controller setup for signup closes #5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :password_confirmation, presence: true, on: :create
+  validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
