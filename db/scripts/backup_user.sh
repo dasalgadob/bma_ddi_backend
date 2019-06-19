9931e4b50f96998e73276e7bcd23c66188e36c6a
Diego Salgado <dasalgadob@unal.edu.co>
2019-06-19 12:21:55 -0500

    Just username for script

--- db/scripts/backup_user.sh
+++ db/scripts/backup_user.sh
@@ -1,1 +1,1 @@
- pg_dump -d ddi_development -t users --data-only --column-inserts -U dasalgadob > users.sql
+ pg_dump -d ddi_development -t users --data-only --column-inserts -U username > users.sql
