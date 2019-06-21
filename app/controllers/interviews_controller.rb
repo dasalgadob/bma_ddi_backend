25047befa4002869d26ebd454238a24fa06533f7
Diego Salgado <dasalgadob@unal.edu.co>
2019-06-21 00:28:38 -0500

    Closes #9 sorting added to index interviews

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,12 +1,12 @@
 class InterviewsController < ApplicationController
   include Pagy::Backend
-  
+  #helper_method :sort_column, :sort_direction
   has_scope :by_name, :by_company, :by_interviewer
   before_action :set_interview, only: [:show, :update, :destroy]
 
   # GET /interviews
   def index
-    @interviews = pagy(apply_scopes(Interview), items: params[:items])
+    @interviews = pagy(apply_scopes(Interview.order(sort_column + " " + sort_direction)), items: params[:items])
     #json_string = InterviewSerializer.new(@interviews[1]).serialized_json
     render json: @interviews
   end
@@ -50,5 +50,13 @@ class InterviewsController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def interview_params
       params.require(:interview).permit(:name, :dimentions, :motivational_questions, :company, :user_id)
+    end
+
+    def sort_column
+      Interview.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
     end
 end
