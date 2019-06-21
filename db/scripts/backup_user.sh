7af78904c3e8cee7b4d0ea1c9ff281643a4147ac
Diego Salgado <dasalgadob@unal.edu.co>
2019-06-20 23:11:27 -0500

    closes #10 backup for interviews

--- db/scripts/backup_user.sh
+++ db/scripts/backup_user.sh
@@ -1,1 +1,3 @@
- pg_dump -d ddi_development -t users --data-only --column-inserts -U username > users.sql
+ pg_dump -d ddi_development -t users --data-only --column-inserts -U username > db/backup/users.sql
+
+  pg_dump -d ddi_development -t interviews --data-only --column-inserts -U username > db/backup/interviews.sql
