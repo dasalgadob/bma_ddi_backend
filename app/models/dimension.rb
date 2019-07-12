10ee9ac5ad8658409fa3805f2588333be1bcfade
Diego Salgado <dasalgadob@unal.edu.co>
2019-07-12 00:10:34 -0500

    Dimensions relationships updated

--- app/models/dimension.rb
+++ app/models/dimension.rb
@@ -1,4 +1,6 @@
 class Dimension < ApplicationRecord
   belongs_to :name, class_name: "Translation"
   belongs_to :description, class_name: "Translation"
+  belongs_to :actions, class_name: "Translation"
+  has_many :questions
 end
