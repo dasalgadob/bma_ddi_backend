cd2f4b1b413c171f9495573fdc0488c93726c718
Diego Salgado <dasalgadob@unal.edu.co>
2019-07-17 18:00:30 -0500

    Making action field in dimension optional

--- app/models/dimension.rb
+++ app/models/dimension.rb
@@ -1,6 +1,6 @@
 class Dimension < ApplicationRecord
   belongs_to :name, class_name: "Translation"
   belongs_to :description, class_name: "Translation"
-  belongs_to :actions, class_name: "Translation"
+  belongs_to :actions, class_name: "Translation", optional: true
   has_many :questions
 end
