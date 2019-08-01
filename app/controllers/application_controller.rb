f67dcfd42191d5b7ca0aa9c71ac43f7bb88b153d
Diego Salgado <dasalgadob@unal.edu.co>
2019-08-01 17:30:56 -0500

    Solved not auth request being made because ActionController::Base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+  protect_from_forgery prepend: true
   #before_action :configure_permitted_parameters, if: :devise_controller?
 
   #protected
