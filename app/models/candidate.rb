73369a8bae7b493b6837d95a6ae82e9e802b83e0
Diego Salgado <dasalgadob@unal.edu.co>
2020-08-10 14:28:17 -0500

    Added candidate model testing

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,6 +1,7 @@
 class Candidate < ApplicationRecord
     validates :name, :email, presence: true
     validates :email, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
     scope :by_name, ->(n) { where("candidates.name ilike ?", '%' +n + '%')}
     scope :by_email, ->(n) { where("candidates.email ilike ?", '%' +n + '%')}
--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Candidate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "is not valid without a name" do
+    expect(Candidate.new(email: "dasalgadob@unal.edu.co")).to_not be_valid
+  end
+
+  it "is not valid without an email" do
+    expect(Candidate.new(name: "Diego")).to_not be_valid
+  end
+
+  it "is valid when name and email are provided" do 
+    expect(Candidate.new(name: "Diego", email: "dasalgadob@unal.edu.co")).to be_valid
+  end
+
+  it "is not valid when email field is not an email" do 
+    expect(Candidate.new(name: "Diego", email: "dasalgadob")).to_not be_valid
+  end
 end
