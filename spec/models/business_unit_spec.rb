8183db88b89d81c581ed418c4215b517dd0c58bc
Diego Salgado <dasalgadob@unal.edu.co>
2020-08-10 14:39:42 -0500

    Validation added to BusinessUnit model

--- spec/models/business_unit_spec.rb
+++ spec/models/business_unit_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe BusinessUnit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is not valid without a code" do
+    expect(BusinessUnit.new(name: "Commercial and Customer Service")).to_not be_valid
+  end
+
+  it "is not valid without the name" do
+    expect(BusinessUnit.new(code: "<3575c318.bmapr.com@amer.teams.ms>")).to_not be_valid
+  end
+
+  it "is valid when code and name are present" do
+    expect(BusinessUnit.new(name: "Commercial and Customer Service", code: "<3575c318.bmapr.com@amer.teams.ms>")).to be_valid
+  end
 end
